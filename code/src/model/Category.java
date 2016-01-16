ad41433228ffa6ed6edb1f08d972e41ea542e7d0
Jan <jan.zenkner@gmail.com>
2016-01-16 23:50:31 +0100

    Added model classes out of ClassDiagramm without DAOs

---
+++ code/src/model/Category.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * Created by Janze on 16.01.2016.
+ */
+public class Category {
+    int id;
+    String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
