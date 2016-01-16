43de5bb0b1e876a2421e1fdbe7b77c55b05e2ce6
Jan <jan.zenkner@gmail.com>
2016-01-17 00:40:17 +0100

    Started to add control classes

---
+++ code/src/control/Control.java
@@ -0,0 +1,7 @@
+package control;
+
+/**
+ * Created by Janze on 17.01.2016.
+ */
+public abstract class Control {
+}
---
+++ code/src/control/RegulationControl.java
@@ -0,0 +1,39 @@
+package control;
+
+import model.TimeSheet;
+import model.User;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Created by Janze on 17.01.2016.
+ */
+public class RegulationControl extends Control {
+    Deque<User> warnedUsers;
+
+    public void checkTimeSheets(LinkedList<TimeSheet> timesheets) {
+
+    }
+    private Deque<User> checkHolidays(LinkedList<TimeSheet> timesheets) {
+
+        return null;
+    }
+    private Deque<User> checkRestrictedTimes(LinkedList<TimeSheet> timesheets){
+
+        return null;
+    }
+    private Deque<User> checkTimeRestrictions(LinkedList<TimeSheet> timesheets) {
+
+        return null;
+    }
+
+    private Deque<User> checkTimeLimits(LinkedList<TimeSheet> timeSheets) {
+
+        return null;
+    }
+
+    private void generateWarnings(Deque<User> users) {
+
+    }
+}
