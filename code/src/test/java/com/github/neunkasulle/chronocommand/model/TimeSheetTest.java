01b8571a9ac573dbca862318ffe4f091b46c7a76
Davraider <davraider@web.de>
2016-01-21 13:05:06 +0100

    add Testcase about TimeSheet

---
+++ code/src/test/java/com/github/neunkasulle/chronocommand/model/TimeSheetTest.java
@@ -0,0 +1,27 @@
+package com.github.neunkasulle.chronocommand.model;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dav on 21.01.2016.
+ */
+public class TimeSheetTest {
+    TimeSheet timeSheet;
+    Proletarier proletTest;
+    Supervisor supervisor;
+    TimeRecord timeRecord;
+
+    @Before
+    public void setUp() throws Exception() {
+        proletTest = new Proletarier("tester", "tester@chrono.command", "pw", "tester", supervisor, 40);
+        timeSheet = new TimeSheet(proletTest, 1, 1, 40);
+    }
+
+    @Test
+    public void testSetTimeSheetState() {
+        //assertFalse(timeSheet.setTimeSheetState());
+        //working on it,  make a break now :)
+    }
+}
