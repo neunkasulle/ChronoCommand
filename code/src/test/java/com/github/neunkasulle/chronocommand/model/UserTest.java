7b79642e8ed3ca3996a516a241076999e2f98e88
Jannis <jannis@sycoso.eu>
2016-01-21 10:12:58 +0100

    Add first test case, add intellij project file

---
+++ code/src/test/java/com/github/neunkasulle/chronocommand/model/UserTest.java
@@ -0,0 +1,27 @@
+package com.github.neunkasulle.chronocommand.model;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new Administrator("blabla", "bla@example.com", "testtest123");
+    }
+
+    @Ignore
+    @Test
+    public void testCheckPasswordCorrect() throws Exception {
+        assertTrue(user.checkPassword("testtest123"));
+    }
+
+    @Test
+    public void testCheckPasswordWrong() throws Exception {
+        assertTrue(user.checkPassword("wrongpassword"));
+    }
+}
