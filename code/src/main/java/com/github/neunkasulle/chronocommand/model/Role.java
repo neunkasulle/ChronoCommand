1187aebb3fbdac07a6092615f635655e4e31676a
Jan <jan.zenkner@gmail.com>
2016-01-23 15:41:05 +0100

    Changed Role so it fits Hibernate and Shiro

--- code/src/main/java/com/github/neunkasulle/chronocommand/model/Role.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/model/Role.java
@@ -1,10 +1,78 @@
 package com.github.neunkasulle.chronocommand.model;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.Index;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.CollectionOfElements;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Set;
+
 /**
  * Created by Janze on 16.01.2016.
  */
-public enum Role {
-    ADMIN,
-    PROLETARIER,
-    SUPERVISOR
+@Entity
+@Table(name="roles")
+@Cache(usage= CacheConcurrencyStrategy.READ_WRITE)
+public class Role {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Set<String> permissions;
+
+    protected Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic(optional = false)
+    @Column(length = 100)
+    @Index(name = "idx_roles_name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic(optional = false)
+    @Column(length = 255)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @CollectionOfElements
+    @JoinTable(name = "roles_permissions")
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+    public Set<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<String> permissions) {
+        this.permissions = permissions;
+    }
 }
