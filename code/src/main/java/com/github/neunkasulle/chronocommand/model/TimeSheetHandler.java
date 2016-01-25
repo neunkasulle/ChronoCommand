56eb2029b6112d0ead60b5ae935fe8dac4f0d9a5
Jannis <jannis@sycoso.eu>
2016-01-25 20:25:34 +0100

    Fix compiler error

--- code/src/main/java/com/github/neunkasulle/chronocommand/model/TimeSheetHandler.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/model/TimeSheetHandler.java
@@ -3,6 +3,8 @@ package com.github.neunkasulle.chronocommand.model;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 
+import java.io.IOException;
+
 /**
  * Created by Janze on 16.01.2016.
  */
@@ -13,18 +15,21 @@ public class TimeSheetHandler {
         return false;
     }*/
 
-    public void createPdfFromTimeSheet(TimeSheet timeSheet){ //TODO fill timesheet with content
+    public void createPdfFromTimeSheet(TimeSheet timeSheet) { //TODO fill timesheet with content
         //new document
         PDDocument newTimesheet = new PDDocument();
         //new page
-        try{
+        try {
             PDPage newPage = new PDPage();
             newTimesheet.addPage(newPage);
 
             //content: userData, timeData
-        }
-        finally {
-            newTimesheet.close(); //most important part!
+        } finally {
+            try {
+                newTimesheet.close(); //most important part!
+            } catch (IOException e) {
+                // TODO handle exception
+            }
         }
     }
 
