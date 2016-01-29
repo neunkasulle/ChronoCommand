6d3c0c1832e92d6c39af02623b93eeaa14a6cc1f
Davraider <davraider@web.de>
2016-01-29 12:14:01 +0100

    added main to test and solved problems

--- code/src/main/java/com/github/neunkasulle/chronocommand/control/SimpleJob.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/control/SimpleJob.java
@@ -13,6 +13,6 @@ public class SimpleJob implements Job {
 
     public void execute(JobExecutionContext context) throws JobExecutionException {
             // Say Hello to the World and display the date/time
-            System.out.println("Hello World! - " + new Date());
+            System.out.println("TRIGGERED");
     }
 }
