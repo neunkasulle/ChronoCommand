b16ed3bb0a5a807e5003d1698c215a4bd1dbece8
Jannis <jannis@sycoso.eu>
2016-02-08 11:52:37 +0100

    Support roles

--- code/src/main/java/com/github/neunkasulle/chronocommand/control/UserManagementControl.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/control/UserManagementControl.java
@@ -1,7 +1,10 @@
 package com.github.neunkasulle.chronocommand.control;
 
-import com.github.neunkasulle.chronocommand.model.User;
+import com.github.neunkasulle.chronocommand.model.*;
+import org.apache.shiro.SecurityUtils;
 import org.hibernate.cfg.NotYetImplementedException;
+
+import java.util.List;
 
 /**
  * Created by Janze on 18.01.2016.
@@ -37,7 +40,13 @@ public class UserManagementControl extends  Control {
         throw new NotYetImplementedException();
     }
 
-
-
+    public List<User> getUsersByRole(Role role) throws ChronoCommandException {
+        User currentUser = LoginControl.getInstance().getCurrentUser();
+        Role administrator = UserDAO.getInstance().getRoleByName(MainControl.ROLE_ADMINISTRATOR);
+        if (!SecurityUtils.getSubject().isPermitted(Role.PERM_ADMINISTRATOR)) {
+            throw new ChronoCommandException(Reason.NOTPERMITTED);
+        }
+        return UserDAO.getInstance().getUsersByRole(role);
+    }
 
 }
