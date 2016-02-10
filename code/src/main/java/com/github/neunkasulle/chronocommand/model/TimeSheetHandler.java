24b562e22653c7f07054c47ae4e2d27b197b3da9
Davraider <davraider@web.de>
2016-02-10 12:41:00 +0100

    exchanged console output with Logger

--- code/src/main/java/com/github/neunkasulle/chronocommand/model/TimeSheetHandler.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/model/TimeSheetHandler.java
@@ -17,12 +17,18 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Created by Janze on 16.01.2016.
  */
 @Entity
 public class TimeSheetHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TimeSheetHandler.class);
+
     @Id
     @GeneratedValue
     Long id;
@@ -55,110 +61,103 @@ public class TimeSheetHandler {
      */
     //TODO @Dav add new page when timesheet is "full" and check if "tätigkeit" needs a new line
     public File createPdfFromTimeSheet(TimeSheet timeSheet) {
-        PDDocument pdfTimeSheet = null;
+        PDDocument pdfTimeSheet;
         File file;
-        File returnFile = null;
+        File returnFile;
         try {
             file = new File(getClass().getClassLoader().getResource("Stundenzettel.pdf").getFile());
-            //returnFile = new File(getClass().getClassLoader().getResource("Study.pdf").getFile());
+            //returnFile = new File(getClass().getClassLoader().getResource("Study.pdf").getFile());//TODO create new file or save different
             returnFile = new File("C:\\Users\\Dav\\Documents\\ChronoCommand\\code\\src\\main\\resources\\Study.pdf");
             pdfTimeSheet = PDDocument.load(file);
         } catch (Exception e) {
-            System.err.println("Loading error.");
+            LOGGER.error("Loading error.");
             return null;
         }
         try {
             PDPage page = pdfTimeSheet.getPage(0);
             PDPageContentStream contents = new PDPageContentStream(pdfTimeSheet, page, true, true);
-            //fillContent(contents, timeSheet);
+
             //TODO
             List<TimeRecord> recordsToPDF = TimeSheetDAO.getInstance().getTimeRecords(timeSheet);
 
             int yOff = 0;
             PDFont font = PDType1Font.HELVETICA;
             PDFont fontBold = PDType1Font.HELVETICA_BOLD;
+            //fill Content
+            contents.beginText();
+            contents.setFont(font, 12);
+            //fill name etc
+            contents.newLineAtOffset(465, 764);
+            contents.showText(Integer.toString(timeSheet.month.getValue()));//month
+            contents.setFont(fontBold, 12);
+            contents.showText(" / ");
+            contents.setFont(font, 12);
+            contents.showText(Integer.toString(timeSheet.year));//year
+            contents.newLineAtOffset(-187, -22);
+            contents.showText(timeSheet.getUser().realname);//name
+            contents.newLineAtOffset(0, -42);
+            contents.showText(timeSheet.getRequiredHoursPerMonth() + " Stunden");//required hours per month
+            contents.endText();
 
-            try {
+            yOff = 637;
+            int sumHour = 0;
+            int sumMin = 0;
+            for (TimeRecord timeR : recordsToPDF) {
                 contents.beginText();
-                contents.setFont(font, 12);
-                //fill name etc
-                contents.newLineAtOffset(465, 764);
-                contents.showText(Integer.toString(timeSheet.month.getValue()));//month
-                contents.setFont(fontBold, 12);
-                contents.showText(" / ");
-                contents.setFont(font, 12);
-                contents.showText(Integer.toString(timeSheet.year));//year
-                contents.newLineAtOffset(-187, -22);
-                contents.showText(timeSheet.getUser().realname);//name
-                contents.newLineAtOffset(0, -42);
-                contents.showText(timeSheet.getRequiredHoursPerMonth() + " Stunden");//required hours per month
-                contents.endText();
+                contents.setFont(font, 10);
+                contents.newLineAtOffset(60, yOff);
+                //contents.showText(timeR.getDescription());//description
 
-                yOff = 637;
-                int sumHour = 0;
-                int sumMin = 0;
-                for (TimeRecord timeR : recordsToPDF) {
-                    contents.beginText();
-                    contents.setFont(font, 10);
-                    contents.newLineAtOffset(60, yOff);
-                    contents.showText(timeR.getDescription());//description
-                    contents.newLineAtOffset(137, 0);//x = 197 from 0
-                    String category = timeR.getCategory().getName();
-                    if (category != null) {
-                        contents.showText(timeR.getCategory().getName());//category
-                    } else {
-                        contents.showText("");
-                    }
-                    //start date + time
-                    LocalDateTime startDate = timeR.getBeginning();
-                    contents.newLineAtOffset(102, 0);//x = 299 from 0
-                    contents.showText(startDate.getDayOfMonth() + "." + startDate.getMonth().getValue() + "." + startDate.getYear());
-                    contents.newLineAtOffset(45, 0);
-                    contents.showText(" " + startDate.getHour() + ":" + startDate.getMinute());
-                    //end date + time
-                    LocalDateTime endDate = timeR.getEnd();
-                    contents.newLineAtOffset(42, 0);
-                    contents.showText(endDate.getDayOfMonth() + "." + endDate.getMonth().getValue() + "." + endDate.getYear());
-                    contents.newLineAtOffset(45, 0);
-                    contents.showText(" " + endDate.getHour() + ":" + endDate.getMinute());
-                    //total time
-                    contents.newLineAtOffset(60, 0);
-                    contents.showText(timeR.getTotHour() + ":" + timeR.getTotMin() + "h");
-                    sumHour += timeR.getTotHour();
-                    sumMin += timeR.getTotMin();
-                    contents.endText();
-                    yOff -= 17;
+                contents.showText("Abrakadabra es erscheine TEXT");
+
+                contents.newLineAtOffset(137, 0);
+                String category = timeR.getCategory().getName();
+                if (category != null) {
+                    contents.showText(timeR.getCategory().getName());//category
+                } else {
+                    contents.showText("");
                 }
-                contents.beginText();
-                contents.newLineAtOffset(384, yOff);
-                contents.setFont(fontBold, 10);
-                contents.showText("Summe");
-                contents.newLineAtOffset(105, 0);
-                contents.showText(sumHour + ":" + sumMin + "h");//sum of total time
+                //start date + time
+                LocalDateTime startDate = timeR.getBeginning();
+                contents.newLineAtOffset(102, 0);
+                contents.showText(startDate.getDayOfMonth() + "." + startDate.getMonth().getValue() + "." + startDate.getYear());
+                contents.newLineAtOffset(45, 0);
+                contents.showText(" " + startDate.getHour() + ":" + startDate.getMinute());
+                //end date + time
+                LocalDateTime endDate = timeR.getEnd();
+                contents.newLineAtOffset(42, 0);
+                contents.showText(endDate.getDayOfMonth() + "." + endDate.getMonth().getValue() + "." + endDate.getYear());
+                contents.newLineAtOffset(45, 0);
+                contents.showText(" " + endDate.getHour() + ":" + endDate.getMinute());
+                //total time
+                contents.newLineAtOffset(60, 0);
+                contents.showText(timeR.getTotHour() + ":" + timeR.getTotMin() + "h");
+                sumHour += timeR.getTotHour();
+                sumMin += timeR.getTotMin();
                 contents.endText();
-
-                contents.close();
-            } catch (Exception e) {
-                System.out.println("Error in fillContent");
+                yOff -= 17;
             }
-            //TODO
+            contents.beginText();
+            contents.newLineAtOffset(384, yOff);
+            contents.setFont(fontBold, 10);
+            contents.showText("Summe");
+            contents.newLineAtOffset(105, 0);
+            contents.showText(sumHour + ":" + sumMin + "h");//sum of total time
+            contents.endText();
+
+            contents.close();
 
             pdfTimeSheet.save(returnFile);
         } catch (Exception e) {
-            System.err.println("problem in content section");
+            LOGGER.error("problem in content section");
         }
         try {
             pdfTimeSheet.close();
         } catch (Exception e) {
-            System.err.println("closing went wrong");
+            LOGGER.error("closing went wrong");
         }
         return returnFile;
     }
-
-    //private void fillContent( PDPageContentStream contents, TimeSheet timeSheet) {
-
-    //    return;
-    //}
 
     /**
      * creates one pdf with all given timesheets
@@ -175,7 +174,7 @@ public class TimeSheetHandler {
             try {
                 doc = PDDocument.load(tmp);
             } catch (Exception e) {
-                System.err.println("Loading error in createPdfFromAllTimeSheets");
+                LOGGER.error("Loading error in createPdfFromAllTimeSheets");
             }
             PDPageTree loopTree =  doc.getPages();
             for (int i = 0; i < loopTree.getCount(); i++) {
