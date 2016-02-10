bb9c3fc03fcd52a61bd8ae8c5272a429cca01a33
Davraider <davraider@web.de>
2016-02-10 11:00:27 +0100

    send monthly reminder to all user which can have a timesheet

--- code/src/main/java/com/github/neunkasulle/chronocommand/control/MonthlyReminder.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/control/MonthlyReminder.java
@@ -1,5 +1,6 @@
 package com.github.neunkasulle.chronocommand.control;
 
+import com.github.neunkasulle.chronocommand.model.Role;
 import com.github.neunkasulle.chronocommand.model.User;
 import com.github.neunkasulle.chronocommand.model.UserDAO;
 import org.quartz.Job;
@@ -16,7 +17,11 @@ public class MonthlyReminder implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         String message = "Last day to check out your time sheet";
-        List<User> supervisor = UserDAO.getInstance().getUsersByRole(UserDAO.getInstance().getRoleByName("supervisor"));//TODO wait for #15
+        List<User> proletarier = UserDAO.getInstance().getUsersByRole(UserDAO.getInstance().getRoleByName(Role.PERM_PROLETARIER));
+        for (User user : proletarier) {
+            TimeSheetControl.getInstance().sendEmail(user, message);
+        }
+        List<User> supervisor = UserDAO.getInstance().getUsersByRole(UserDAO.getInstance().getRoleByName(Role.PERM_SUPERVISOR));
         for (User user : supervisor) {
             TimeSheetControl.getInstance().sendEmail(user, message);
         }
