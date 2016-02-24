fe3f841a4655897d24cd52fcae543d6571db7315
Davraider <davraider@web.de>
2016-02-24 14:51:21 +0100

    only supervisor with timesheet get a reminder to send in time sheet now

--- code/src/main/java/com/github/neunkasulle/chronocommand/control/MonthlyReminder.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/control/MonthlyReminder.java
@@ -1,12 +1,11 @@
 package com.github.neunkasulle.chronocommand.control;
 
-import com.github.neunkasulle.chronocommand.model.Role;
-import com.github.neunkasulle.chronocommand.model.User;
-import com.github.neunkasulle.chronocommand.model.UserDAO;
+import com.github.neunkasulle.chronocommand.model.*;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -18,13 +17,18 @@ public class MonthlyReminder implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         String message = "Please remember to send in your time sheet for this month.";
+
         List<User> proletarier = UserDAO.getInstance().getUsersByRole(UserDAO.getInstance().getRoleByName(Role.PERM_PROLETARIER));
         for (User user : proletarier) {
             TimeSheetControl.getInstance().sendEmail(user, message);
         }
         List<User> supervisor = UserDAO.getInstance().getUsersByRole(UserDAO.getInstance().getRoleByName(Role.PERM_SUPERVISOR));
         for (User user : supervisor) {
-            TimeSheetControl.getInstance().sendEmail(user, message);
+            TimeSheet sheet = TimeSheetDAO.getInstance().getLatestTimeSheet(user);
+            LocalDateTime now = LocalDateTime.now();
+            if (sheet.getMonth() == now.getMonth()) {
+                TimeSheetControl.getInstance().sendEmail(user, message);
+            }
         }
     }
 }
