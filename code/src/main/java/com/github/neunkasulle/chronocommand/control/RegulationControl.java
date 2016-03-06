d97416a37a662ea100a05c7b9a8bdc5210245a9b
Jan <jan.zenkner@gmail.com>
2016-03-06 19:27:28 +0100

    remove yet not implemented methods

--- code/src/main/java/com/github/neunkasulle/chronocommand/control/RegulationControl.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/control/RegulationControl.java
@@ -1,18 +1,10 @@
 package com.github.neunkasulle.chronocommand.control;
 
-import com.github.neunkasulle.chronocommand.model.TimeSheet;
-import com.github.neunkasulle.chronocommand.model.User;
-import org.hibernate.cfg.NotYetImplementedException;
-
-import java.util.Deque;
-import java.util.List;
-
 /**
  * Created by Janze on 17.01.2016.
  * Checks Time Sheets for regulation violations
  */
 public class RegulationControl {
-    Deque<User> warnedUsers;
 
     private static RegulationControl ourInstance = new RegulationControl();
 
@@ -27,16 +19,6 @@ public class RegulationControl {
      */
     public static RegulationControl getInstance() {
         return ourInstance;
-    }
-
-
-    /**
-     * Checks timeSheets for incossitencies and rule violations
-     *
-     * @param timesheets TimeSheets which are to be checked
-     */
-    public void checkTimeSheets(List<TimeSheet> timesheets) {
-        throw new NotYetImplementedException();
     }
 
 }
