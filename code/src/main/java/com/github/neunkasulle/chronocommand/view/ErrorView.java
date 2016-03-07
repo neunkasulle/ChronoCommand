2f6572276743e11e824be0afdce3123b00f48ba9
Jannis <jannis@sycoso.eu>
2016-03-07 10:18:17 +0100

    Add button to error view

--- code/src/main/java/com/github/neunkasulle/chronocommand/view/ErrorView.java
+++ code/src/main/java/com/github/neunkasulle/chronocommand/view/ErrorView.java
@@ -2,6 +2,7 @@ package com.github.neunkasulle.chronocommand.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -12,5 +13,10 @@ public class ErrorView extends VerticalLayout implements View {
         Label error = new Label("Failed to find requested site: " + event.getViewName());
         error.setStyleName(ValoTheme.LABEL_H1);
         addComponent(error);
+
+        Button link = new Button("Please go to the login page");
+        link.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+        addComponent(link);
+        link.addClickListener(event1 -> getUI().getNavigator().navigateTo(MainUI.LOGINVIEW));
     }
 }
